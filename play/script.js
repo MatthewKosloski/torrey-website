5b0e011549408165c6943bcc1c24503819983d23
Matthew Kosloski <matthewkosloski@me.com>
2021-07-12 00:16:48 -0500

    feat: replaced radio buttons with a dropdown (#1)
    * chore: test deployment of dev branch
    
    * feat: replaced radio buttons with flag dropdown
    
    * fix: call lambda function
    
    * feat: updated home page

--- play/script.js
+++ play/script.js
@@ -34,50 +34,33 @@
     versionSelectContainer.disabled = false;
 
     // Populate control with compiler flag radio buttons.
-    supportedFlags.forEach((flag) => {
-        const formCheck = document.createElement('div');
-        formCheck.setAttribute('class', 'form-check form-check-inline');
-        const formCheckInput = document.createElement('input');
-        formCheckInput.setAttribute('id', `torrey-flag${flag}`);
-        formCheckInput.setAttribute('class', 'form-check-input');
-        formCheckInput.setAttribute('type', 'radio');
-        formCheckInput.setAttribute('name', 'torrey-flag');
-        formCheckInput.setAttribute('value', flag);
+    supportedFlags.forEach(({name, description}) => {
+        const option = document.createElement('option');
+        option.value = name;
+        option.textContent = description;
 
-        if (defaultFlags.includes(flag))
-            formCheckInput.checked = true;
-        
-        const formCheckLabel = document.createElement('label');
-        formCheckLabel.setAttribute('class', 'form-check-label');
-        formCheckLabel.setAttribute('for', `torrey-flag${flag}`);
-        formCheckLabel.textContent = flag;
-        formCheck.appendChild(formCheckInput);
-        formCheck.appendChild(formCheckLabel);
-        flagSelectContainer.appendChild(formCheck);
+        if (defaultFlags.includes(name))
+            option.selected = true;
+
+        flagSelectContainer.appendChild(option);
     });
+
+    flagSelectContainer.disabled = false;
 
     const btnRun = document.querySelector('#js-run');
 
 
     btnRun.addEventListener('click', () => {
-
-        let flags;
-        supportedFlags.forEach((flag) => {
-            if (document.querySelector(`#torrey-flag${flag}`).checked)
-              flags = [flag];
-        });
-
+        const flag = document.querySelector('#js-flag-select').value;
         const semanticVersion = document.querySelector('#js-version-select').value;
+
+        let flags = [];
+        if (flag) flags.push(flag);
 
         const body = JSON.stringify({
             program: document.querySelector('#js-textarea-input').value.trim(),
-            options: {
-                flags,
-                semanticVersion 
-            }
+            options: { flags, semanticVersion }
         });
-
-        // console.log('sending this to lambda', body);
 
         fetch('https://yo6quyed5e.execute-api.us-east-1.amazonaws.com/test/compile', {
             method: 'POST',
@@ -85,8 +68,6 @@
             body
         }).then(res => res.json())
         .then((json) => console.log('lambda response', json));
-
-
     });
 
 })();
