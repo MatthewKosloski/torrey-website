3e0d10ed1dfb3482aa14fd01cd3889ebef757140
Matthew Kosloski <matthewkosloski@me.com>
2021-07-12 23:45:23 -0500

    fix: add newline after text

--- play/script.js
+++ play/script.js
@@ -72,7 +72,7 @@
             if (json.stdout) {
                 const span = document.createElement('span');
                 span.setAttribute('class', 'torrey-console__stdout');
-                span.textContent = (json.stderr + "\n").trim();
+                span.textContent = json.stdout.trim() + "\n";
                 console.appendChild(span);
                 const cursor = document.createElement('span');
                 cursor.innerHTML = '&gt;';
@@ -81,7 +81,7 @@
             if (json.stderr) {
                 const span = document.createElement('span');
                 span.setAttribute('class', 'torrey-console__stderr');
-                span.textContent = (json.stderr + "\n").trim();
+                span.textContent = json.stderr.trim() + "\n";
                 console.appendChild(span);
                 const cursor = document.createElement('span');
                 cursor.innerHTML = '&gt;';
